8c3365f359be8edf5e9df8674311204e76ac8a29
icy-r <asath12882@gmail.com>
2024-08-11 11:46:36 +0530

    initial commit

---
+++ app/src/main/java/com/example/try1/LaunchActivity1.kt
@@ -0,0 +1,33 @@
+package com.example.try1
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class LaunchActivity1:AppCompatActivity() {
+    lateinit var nextBtn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_launch1)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        nextBtn = findViewById(R.id.next1)
+        nextBtn.setOnClickListener {
+            val intent = Intent(this, LaunchActivity2::class.java).also {
+                startActivity(it)
+            }
+            startActivity(intent)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/try1/LaunchActivity3.kt
@@ -0,0 +1,33 @@
+package com.example.try1
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LaunchActivity3:AppCompatActivity() {
+    lateinit var nextBtn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_launch3)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        nextBtn = findViewById(R.id.next2)
+        nextBtn.setOnClickListener {
+            val intent = Intent(this, LaunchActivity3::class.java).also {
+                startActivity(it)
+            }
+            startActivity(intent)
+        }
+
+    }
+
+}
